bda0df7f4fe909ed6f1d63e19fe11aecd5bb0b55
Hieu2895 <hieu2895@gmail.com>
2021-10-23 09:50:57 +0700

    update try catch and using singleton

--- src/main/java/com/example/controller/AddProductControl.java
+++ src/main/java/com/example/controller/AddProductControl.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.example.model.Account;
+import com.example.model.Category;
 import com.example.model.Product;
 import com.example.service.CategoryService;
 import com.example.service.ProductService;
@@ -19,33 +20,40 @@ public class AddProductControl extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        req.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        String image = req.getParameter("image");
-        String price = req.getParameter("price");
-        String title = req.getParameter("title");
-        String description = req.getParameter("description");
-        String category = req.getParameter("category");
+        try {
+            req.setCharacterEncoding("UTF-8");
+            String name = req.getParameter("name");
+            String image = req.getParameter("image");
+            String price = req.getParameter("price");
+            String title = req.getParameter("title");
+            String description = req.getParameter("description");
+            String category = req.getParameter("category");
 
-        HttpSession session = req.getSession();
-        Account account = (Account) session.getAttribute("acc");
+            HttpSession session = req.getSession();
+            Account account = (Account) session.getAttribute("acc");
 
-        CategoryService categoryService = new CategoryService();
-        ProductService productService = new ProductService();
+            CategoryService categoryService = CategoryService.getInstance();
+            ProductService productService = ProductService.getInstance();
 
-        Product product = new Product();
-        product.setName(name);
-        product.setTitle(title);
-        product.setPrice(Double.parseDouble(price));
-        product.setDescription(description);
-        product.setImage(image);
-        product.setAccount(account);
-        product.setCategory(categoryService.findCategoryById(Integer.parseInt(category)));
+            Category setCategory = categoryService.findCategoryById(Integer.parseInt(category));
 
-        productService.addNewProduct(product);
+            Product product = new Product();
+            product.setName(name);
+            product.setTitle(title);
+            product.setPrice(Double.parseDouble(price));
+            product.setDescription(description);
+            product.setImage(image);
+            product.setAccount(account);
+            product.setCategory(setCategory);
 
-        String tag = req.getParameter("tag");
-        req.getRequestDispatcher("paging?id=" + tag).forward(req, resp);
+            productService.addNewProduct(product);
+
+            String tag = req.getParameter("tag");
+            req.getRequestDispatcher("paging?id=" + tag).forward(req, resp);
+
+        } catch (Exception ex) {
+
+        }
 
     }
 }
